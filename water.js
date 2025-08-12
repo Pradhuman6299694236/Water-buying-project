4338569ce7c1b45c7f9fb4f2145a843304de737e
Pradhuman6299694236 <pradhumanprasadgp@gmail.com>
2025-08-12 19:01:53 +0530

    adding distributor section

--- water.js
+++ water.js
@@ -1,50 +1,98 @@
 // Handle CTA button click
 
-
+window.onload = () => {
+  window.scrollTo(0, 0);
+}
 // Handle form submission
-document.getElementById('contact-form').addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent default form submission
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
 
-  if (name && email) {
-    alert(`Thank you, ${name}! We'll contact you at ${email}.`);
-    document.getElementById('contact-form').reset();
-  } else {
-    alert('Please fill in all fields.');
-  }
-});
 
 let buyButtons = document.querySelectorAll(".add-to-cart");
 let ctaButton = document.querySelector("#cta-button");
+let registrationButton = document.querySelector("#distributor");
+let contactForm = document.getElementById('contact-form');
 
 
-  ctaButton.addEventListener("click", () => {
-    const phoneNumber = "6299694236";
-    const message = encodeURIComponent("Hello, I am order 20 ltr water provide me according to the policy within 15 min...");
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
-    window.location.href = whatsappUrl;
-});
 
-
-let getUserLocation = ()=>{
-  if(navigator.geolocation){
+let getDistributorLocation = async () => {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       //success callback
-      (position)=>{
+      (position) => {
         console.log(`Latitude: ${position.coords.latitude}, Longitude : ${position.coords.longitude}`);
+        window.location.href = "distributor.html";
       },
-      (error)=>{
+      (error) => {
         console.error(`Error : ${error.message}`);
       },
 
       {timeout: 20000, enableHighAccuracy: true}
     )
   }
-}
+};
 
-buyButtons.forEach((button)=>{
-  button.addEventListener("click",()=>{
-  getUserLocation();
-})
-})
+let getUserLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      //success callback
+      (position) => {
+        console.log(`Latitude: ${position.coords.latitude}, Longitude : ${position.coords.longitude}`);
+
+      },
+      (error) => {
+        console.error(`Error : ${error.message}`);
+      },
+
+      { timeout: 20000, enableHighAccuracy: true }
+    )
+  }
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  if (contactForm) {
+    contactForm.addEventListener('submit', (e) => {
+      e.preventDefault(); // Prevent default form submission
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+
+      if (name && email) {
+        alert(`Thank you, ${name}! We'll contact you at ${email}.`);
+        document.getElementById('contact-form').reset();
+      } else {
+        alert('Please fill in all fields.');
+      }
+    });
+  };
+  if (ctaButton) {
+    ctaButton.addEventListener("click", () => {
+      const phoneNumber = "6299694236";
+      const message = encodeURIComponent("Hello, I am order 20 ltr water provide me according to the policy within 15 min...");
+      const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+      window.location.href = whatsappUrl;
+    });
+  };
+
+  if (buyButtons) {
+    buyButtons.forEach((button) => {
+      button.addEventListener("click",() => {
+        getUserLocation();
+      })
+    });
+  }
+
+  
+  if (registrationButton) {
+    registrationButton.addEventListener("click", async () => {
+      await getDistributorLocation();
+      
+    });
+  };
+});
+
+
+
+
+
+
+
